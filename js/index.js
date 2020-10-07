564393be6fad57ab13857dbebdee8545736d73ba
Osmian101 <r093r25@gmail.com>
2020-10-06 22:23:49 -0400

    it doesnt work

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-import * as THREE from 'three';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
-
 const scene = new THREE.Scene();
 var loader = new GLTFLoader();
 loader.load('assets\models\floorplan.glb', function (gtlf){scene.add(gtlf.scene)}, undefined, function(error){console.error(error)});
